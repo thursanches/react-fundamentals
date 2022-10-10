9eae9b61c3b5f9101fa4cc93fb0c1121350fa6ed
Arthur Sanches <arthursks7@gmail.com>
2022-10-10 11:04:24 -0300

    Parte 2 - Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -20,7 +22,10 @@ export function Sidebar() {
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil.</a>
+        <a href="#">
+          <PencilLine />
+          Editar seu perfil.
+        </a>
       </footer>
     </aside>
   );

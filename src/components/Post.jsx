b1ab8ae199fa7b51a295317a7b5602f03df79f53
Arthur Soares <arthursks7@gmail.com>
2023-11-26 22:30:24 -0300

    avatar border config

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/thursanches.png"
-          />
+          <Avatar src="https://github.com/thursanches.png" />
           <div className={styles.authorInfo}>
             <strong>Arthur</strong>
             <span>Software Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "phosphor-react";
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -11,11 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://avatars.githubusercontent.com/u/66748313?v=4"
-          alt=""
-        />
+        <Avatar src="https://www.github.com/maykbrito.png" />
 
         <strong>Arthur Sanches</strong>
         <span>Developer</span>

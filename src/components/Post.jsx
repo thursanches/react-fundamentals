93371009e2e47a9c11d51bdc92a4a9e22129670b
Arthur Soares <arthursks7@gmail.com>
2023-11-27 19:09:24 -0300

    iteration

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post(props) {
   return (
     <article className={styles.post}>
       <header>
@@ -19,23 +19,7 @@ export function Post() {
         </time>
       </header>
 
-      <div className={styles.content}>
-        <p>Fala pessoal 👋👋</p>
-
-        <p>
-          Acabei de subir mais um projeto no meu portifólio. É um projeto que
-          fiz no NLW Return, evento da Rocketseat. O nome do projeto é
-          DoctorCare 🚀
-        </p>
-
-        <p>
-          👉 <a href="">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
-          <a href="">#rocketseat</a>{" "}
-        </p>
-      </div>
+      <div className={styles.content}></div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>

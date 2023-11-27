d24c58471144a108a90c29ad885ebcf9bcfc6ca0
Arthur Soares <arthursks7@gmail.com>
2023-11-27 19:09:56 -0300

    iteration

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/thursanches.png",
+      name: "Arthur Sanches",
+      role: "Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala pessoal 👋👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-11-27 18:12:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/thursanches.png",
+      name: "Lisa",
+      role: "Educator",
+    },
+    content: [
+      { type: "paragraph", content: "Fala pessoal 👋👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-11-01 18:12:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,15 +53,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Arthur"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur tempora fuga eum? Nam laborum quaerat voluptates quisquam, nihil est blanditiis id porro? Blanditiis rem, molestiae cum quod est impedit autem!"
-          />
-
-          <Post
-            author="Sanches"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur tempora fuga eum? Nam laborum quaerat voluptates quisquam, nihil est blanditiis id porro? Blanditiis rem, molestiae cum quod est impedit autem!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>

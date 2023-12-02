0cc4c393025015f976c9c2db30082264f709c99e
Arthur Soares <arthursks7@gmail.com>
2023-12-02 10:30:23 -0300

    declarative programming

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,10 +2,14 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/thursanches.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/thursanches.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -22,7 +26,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom, sucesso!!</p>
+          <p>{content}</p>
         </div>
 
         <footer>

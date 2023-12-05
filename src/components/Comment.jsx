10227c412f5f096ff0b1bf7f72b1731ee0dd8c75
Arthur Soares <arthursks7@gmail.com>
2023-12-05 17:08:19 -0300

    Definition of like reaction

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,21 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
   }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar
@@ -33,9 +43,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>18</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
